a49a045fc419209c2f940375f37b4bd5c30f9356
SusieHatter <susiivaldez@gmail.com>
2021-09-04 22:58:24 -0500

    Fix hurtbox bug, add camera limits

--- hitboxes and hurtboxes/Hurtbox.gd
+++ hitboxes and hurtboxes/Hurtbox.gd
@@ -4,7 +4,8 @@ const HitEffect = preload("res://Effects/HitEffect.tscn")
 
 var invincible = false setget set_invincible
 
-onready var timer = $Timer
+onready var timer = $Timer 
+onready var collisionShape = $CollisionShape2D
 
 signal invincibility_started
 signal invincibility_ended
@@ -31,7 +32,8 @@ func _on_Timer_timeout():
 	self.invincible = false
 
 func _on_Hurtbox_invincibility_started():
-	set_deferred("monitoring", false)
+	collisionShape.set_deferred("disabled", true)
 
 func _on_Hurtbox_invincibility_ended():
-	monitoring = true
+	collisionShape.disabled = false
+	
